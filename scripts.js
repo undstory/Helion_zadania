fa127ccc72b37a0edbbbab21ddc51522e0c18cca
undstory <undrul.ozog@gmail.com>
2021-08-21 12:27:10 +0200

    sort is on!

--- scripts.js
+++ scripts.js
@@ -1,28 +1,65 @@
-import drawHeader from "./helpers/drawHeader.js";
+
 import drawContent from "./helpers/drawContent.js";
 
-const tbody = document.querySelector('tbody');
-const thead = document.querySelector('thead');
-const button = document.querySelector('button');
 
+const tbody = document.querySelector('tbody');
+// const thead = document.querySelector('thead');
+const button = document.querySelector('button');
+const ths = [...document.querySelectorAll("thead th")];
+
+
+const sortBy = (e) => {
+    let target = e.target
+    const trs = [...document.querySelectorAll("tbody tr")];
+    let index = ths.indexOf(target);
+    console.log(index);
+    const df = document.createDocumentFragment();
+
+    trs.sort(function(a, b) {
+        const tdA = a.children[index].textContent;
+        const tdB = b.children[index].textContent;
+
+        if(tdA < tdB) {
+            return -1;
+        } else if(tdA > tdB) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
+
+    trs.forEach(function(tr){
+        df.appendChild(tr);
+    });
+
+ tbody.appendChild(df);
+ 
+}
+
+for(let i=0; i < ths.length; i++){
+
+    ths[i].addEventListener('click', sortBy) // na kazdym elemencie th wywołaj funkcje sortBy, jest to zdarzenie więc można przechwycić event e 
+}
 
 const drawTable = (books) => {
+    
     tbody.textContent = '';
-    thead.textContent = '';
-    thead.appendChild(drawHeader());
     books.forEach(book => {
+   
         tbody.appendChild(drawContent(book))
     });
+
+ 
+}
+
+
+
+const getList = (myData) => {
+    const { books } = myData;
+    
+    drawTable(books);
    
 }
-
-
-
-const getList = (data) => {
-    const books = data;
-    drawTable(books);
-}
-
 
 
 button.addEventListener('click', function(){
@@ -30,3 +67,4 @@ button.addEventListener('click', function(){
     .then(res => res.json())
     .then(json => getList(json))
 })
+
